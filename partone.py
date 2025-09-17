49c65daa2ff5aac742c1cede9dd491fe8fa996e9
olivia l <olivialu415@gmail.com>
2025-09-17 10:17:06 -0400

    Create partone.py

---
+++ partone.py
@@ -0,0 +1,5 @@
+list.ItemList = ["Gooner Fanfic", "Study Materials", "Diary of a Rat", "Teriyaki One Menu", "Three Week Old Moldy Ahh Cheese"]
+
+print(f"Welcome to Bigbackliver's Library!! Today we have {ItemList} for sale.")
+
+userWant = input("What would you like to buy?")
