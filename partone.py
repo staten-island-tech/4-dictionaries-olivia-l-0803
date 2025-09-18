7150956f5af51d7e1d1acadd6f24df7ea7601d1f
olivia l <olivialu415@gmail.com>
2025-09-18 10:05:04 -0400

    Update partone.py

--- partone.py
+++
@@ -1,5 +0,0 @@
-list.ItemList = ["Gooner Fanfic", "Study Materials", "Diary of a Rat", "Teriyaki One Menu", "Three Week Old Moldy Ahh Cheese"]
-
-print(f"Welcome to Bigbackliver's Library!! Today we have {ItemList} for sale.")
-
-userWant = input("What would you like to buy?")
