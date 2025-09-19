641a2811d3ebe6f5f5a2db3327127f2225e3cca8
olivia l <olivialu415@gmail.com>
2025-09-19 09:32:59 -0400

    Update partone.py

--- partone.py
+++ partone.py
@@ -9,16 +9,16 @@ purchases = ["Gooner Fanfic", "Study Materials", "Teriyaki One Menu", "Three Wee
 
 print(f"Welcome to Bigbackliver's Library!! Today we have {purchases[0]}, {purchases[1]}, {purchases[2]}, and {purchases[3]}  for sale.")
 
-userWant = input("What would you like to buy?")
+userWant = input("What would you like to buy? ").lower()
 
 
 
 
-if userWant == "Gooner Fanfic" or "gooner fanfic" or "item 1":
-    print(f"Let's buy {ItemList[0]['itemName']} for {ItemList[0]['price']}?")
-elif userWant == "Study Materials":
-    print(f"Let's buy {ItemList[1]['itemName']} for {ItemList[1]['price']}?")
-elif userWant == "Teriyaki One Menu":
-    print(f"Let's buy {ItemList[2]['itemName']} for {ItemList[2]['price']}?")
-elif userWant == "Three Week Old Moldy Ahh Cheese":
-    print(f"Let's buy {ItemList[3]['itemName']} for {ItemList[3]['price']}?")
+if userWant == "gooner fanfic" or "item 1":
+    print(f"Let's buy {ItemList[0]['itemName']} for {ItemList[0]['price']}!")
+elif userWant == "study materials" or "item 2":
+    print(f"Let's buy {ItemList[1]['itemName']} for {ItemList[1]['price']}!")
+elif userWant == "teriyaki one menu" or "item 3":
+    print(f"Let's buy {ItemList[2]['itemName']} for {ItemList[2]['price']}!")
+elif userWant == "three week old moldy ahh cheese":
+    print(f"Let's buy {ItemList[3]['itemName']} for {ItemList[3]['price']}!")
