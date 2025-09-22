df8d4f4c81312a74538f01b9ccb3685132bd3772
olivia l <olivialu415@gmail.com>
2025-09-22 09:51:32 -0400

    Update partone.py

--- partone.py
+++ partone.py
@@ -2,7 +2,7 @@
 ItemList = [{"itemName": "Gooner Fanfic", "price": "$35"},
             {"itemName": "Study Materials", "price": "$500"},
             {"itemName": "Teriyaki One Menu", "price": "$10"},
-            {"itemName": "Three Week Old Moldy Ahh Cheese", "price": "Freebie!"}
+            {"itemName": "Three Week Old Moldy Ahh Cheese", "price": "free!"}
             ]
  
 purchases = ["Gooner Fanfic", "Study Materials", "Teriyaki One Menu", "Three Week Old Moldy Ahh Cheese"]
@@ -14,11 +14,11 @@ userWant = input("What would you like to buy? ").lower()
 
 
 
-if userWant == "gooner fanfic" or "item 1":
+if userWant == "gooner fanfic" or userWant == "item 1":
     print(f"Let's buy {ItemList[0]['itemName']} for {ItemList[0]['price']}!")
-elif userWant == "study materials" or "item 2":
+elif userWant == "study materials" or userWant == "item 2":
     print(f"Let's buy {ItemList[1]['itemName']} for {ItemList[1]['price']}!")
-elif userWant == "teriyaki one menu" or "item 3":
+elif userWant == "teriyaki one menu" or userWant == "item 3":
     print(f"Let's buy {ItemList[2]['itemName']} for {ItemList[2]['price']}!")
-elif userWant == "three week old moldy ahh cheese":
+elif userWant == "three week old moldy ahh cheese" or userWant == "item 4":
     print(f"Let's buy {ItemList[3]['itemName']} for {ItemList[3]['price']}!")
