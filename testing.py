630c1e5699b1ecc47e334bed146754326acf3712
olivia l <olivialu415@gmail.com>
2025-09-29 09:56:53 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,12 @@
 userItemInput = input("Which item do you want to buy? (Type a number): ") 
-
+# input()
 print(type(userItemInput))
 
 
 
-test = input('Type something')
+test = input('Type something largeling ')
 
-if test 
+if test.__contains__('biggie'):
+    print("die kill your")
+else:
+    print("biggie")
